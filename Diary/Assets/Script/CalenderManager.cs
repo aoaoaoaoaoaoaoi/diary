1c161975bc3cdbb9924ef7963bfb391d6eb60eb7
unknown <gummi.bear456aaaaa@gmail.com>
2021-03-29 00:18:32 +0900

    add a calendar

---
+++ Diary/Assets/Script/CalenderManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CalenderManager : MonoBehaviour
+{
+    [SerializeField] Text dateText;
+    [SerializeField] Button prevButton;
+    [SerializeField] Button nextButton;
+    [SerializeField] List<Button> dayButtons;
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
